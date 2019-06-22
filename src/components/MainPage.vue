58cb3e645c38d3a14feb8d35a44969addc8ab356
john <likisongproduct@gmail.com>
2019-06-22 01:04:32 -0700

    body-box grid orientation and responsiveness

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -15,9 +15,11 @@
                     </div>
                     <div class="col-md-6">
                         <div class="text-box">
-                            <p>Hello! I'm Jaehun, currently a student at the University of British Columbia. Over the years I've had the opportunity to travel to many different 
-                                countries and exposure to many new cultures. I created this page as a means to showcase my personal experiences. I hope you enjoy your stay! 
-                                 </p>
+                            <p>Hello! I'm Jaehun, currently a student at the University of British Columbia. Over the
+                                years I've had the opportunity to travel to many different
+                                countries and exposure to many new cultures. I created this page as a means to showcase
+                                my personal experiences. I hope you enjoy your stay!
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -29,22 +31,36 @@
             <div class="container-fluid">
                 <div class="picture-box">
                     <div class="second-picture">
-                        
+
                         <div class="row">
-                            <div class="col-md-6">
-                                <div class="text-box">
-                                    <h1>Reuniting with familiar faces</h1> 
-                                    <p>I recently had the opportunity to go back to my hometown Busan, in South Korea. To be able to reconnect 
-                                        with my family and my culture was an unforgettable experience. 
-                                    </p>
+                            <div class="col-md-6 latest-feed-box">
+                                <div class="text-box-2">
+                                    <div class="latest-feed-title">
+                                        <h1>Reuniting with old faces</h1>
+                                    </div>
+                                    <div class="latest-feed-para">
+                                        <p>I recently had the opportunity to go back to my hometown Busan, in South
+                                            Korea. To be able to reconnect
+                                            with my family and my culture was an unforgettable experience.
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-md-6 video-box">
-                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/zuchgwZ1Abs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/zuchgwZ1Abs"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div class="posts">
+            <div class="sub-title">
+                <h3>Latest Destinations</h3>
             </div>
         </div>
     </div>
@@ -97,11 +113,6 @@
         .intro-box {
             margin-top: 150px;
             margin-bottom: 80px;
-            /* height: 60vh;
-            min-height: 300px;
-            max-height: 400px;
-            display: flex;
-            align-items: center; */
         }
     }
 
@@ -120,9 +131,19 @@
         color: black;
     }
 
-    /* 
-    .body-box {
-        margin: 8%;
+    .text-box-2 {
+        color: rgb(214, 232, 233);
+        display: flex;
+        align-items: center;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 310px;
+    }
+
+
+    /* .body-box {
+        margin-bottom: 30px;
     } */
 
     .profile-pic {
@@ -142,16 +163,43 @@
         justify-content: center;
     }
 
+    .latest-feed-box {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
     .second-picture {
         background-image: url("../body.jpg");
         width: 100%;
         height: 100%;
         background-size: cover;
         background-repeat: no-repeat;
-        filter: grayscale(40%);
     }
 
     .col-md {
         padding: 0px;
+    }
+
+    .video-box {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .row {
+        height: 100%;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    .posts {
+        height: 70vh;
+    }
+
+    .sub-title {
+        margin-top: 50px;
     }
 </style>
