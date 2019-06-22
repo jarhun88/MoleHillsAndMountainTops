e2b71da785435630334fe809692e141f7df8f607
john <likisongproduct@gmail.com>
2019-06-22 00:04:00 -0700

    filled out text-box

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -15,17 +15,9 @@
                     </div>
                     <div class="col-md-6">
                         <div class="text-box">
-                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod
-                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                                nostrud
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                                dolor
-                                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur
-                                sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                est
-                                laborum."</p>
+                            <p>Hello! I'm Jaehun, currently a student at the University of British Columbia. Over the years I've had the opportunity to travel to many different 
+                                countries and exposure to many new cultures. I created this page as a means to showcase my personal experiences. I hope you enjoy your stay! 
+                                 </p>
                         </div>
                     </div>
                 </div>
