fcb79c3f71a511b6a40f6e1b59e290f62a887585
john <likisongproduct@gmail.com>
2019-06-24 23:38:06 -0700

    picture-box responsiveness div fixed

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -13,14 +13,11 @@
                         <img src="../john.jpeg" class="profile-pic" alt="Picture of John" width="304px"
                             height="236px" />
                     </div>
-                    <div class="col-md-6">
-                        <div class="text-box">
-                            <p>Hello! I'm Jaehun, currently a student at the University of British Columbia. Over the
-                                years I've had the opportunity to travel to many different
-                                countries and exposure to many new cultures. I created this page as a means to showcase
-                                my personal experiences. I hope you enjoy your stay!
-                            </p>
-                        </div>
+                    <div class="col-md-6 text-box">
+                        <p>Hello! I'm Jaehun, currently a student at the University of British Columbia. </p>
+                        <p>Recently, I've had the opportunity to travel to many different
+                            countries and exposure to many new cultures. I created this page as a means to showcase
+                            my personal experiences. I hope you enjoy your stay!</p>
                     </div>
                 </div>
             </div>
@@ -31,7 +28,7 @@
         <div class="picture-box">
             <div class="second-picture">
                 <div class="row">
-                    <div class="col-md-4 latest-feed-box">
+                    <div class="col-md-6 latest-feed-box">
                         <div class="text-box-2">
                             <div class="latest-feed-title">
                                 <h1>Reuniting with old faces</h1>
@@ -44,7 +41,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-8 video-box">
+                    <div class="col-md-6 video-box">
                         <iframe src="https://www.youtube.com/embed/zuchgwZ1Abs" frameborder="0"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen></iframe>
@@ -119,7 +116,7 @@
         align-items: center;
     }
 
-    .padding{
+    .padding {
         height: 70px;
     }
 
@@ -138,15 +135,16 @@
     }
 
     .text-box {
+        padding-top: 5%;
         display: flex;
         align-items: center;
         text-align: center;
-        padding: 8%;
+        flex-direction: column;
         color: black;
     }
 
     .text-box-2 {
-        color: rgb(214, 232, 233);
+        color: white;
         display: flex;
         align-items: center;
         text-align: center;
@@ -155,9 +153,6 @@
         margin-bottom: 310px;
     }
 
-
-    
-
     .profile-pic {
         height: 300px;
         width: 300px;
@@ -165,8 +160,51 @@
     }
 
     .picture-box {
-        height: 80vh;
-        min-height: 250px;
+        height: 90vh;
+        min-height: 550px;
+    }
+
+    @media screen and (max-width: 768px) {
+        .picture-box {
+            height: 100vh;
+            min-height: 400px;
+        }
+
+        .second-picture {
+            background-image: url("../body.jpg");
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+
+        .text-box-2 {
+            height: 40%;
+            display: flex;
+            align-items: flex-start;
+            margin-bottom: 0px;
+        }
+
+        .latest-feed-title {
+            position: relative;
+            bottom: 50px;
+        }
+
+        .latest-feed-para {
+            position: relative;
+            bottom: 50px;
+        }
+
+        .video-box {
+            position: relative;
+            bottom: 100px;
+        }
+
+        .iframe {
+            width: 100%;
+            max-height: 400px;
+            height: 100%;
+        }
     }
 
     .profile-box {
@@ -190,9 +228,10 @@
         /* filter: brightness(50%); */
     }
 
-    iframe {
+
+    div > iframe {
         width: 100%;
-        height: 100%;
+        height: 80%;
         max-width: 600px;
         max-height: 400px;
         padding: 20px;
@@ -227,7 +266,6 @@
 
     p {
         font-size: 20px;
-        padding: 20px;
     }
 
     .images {
