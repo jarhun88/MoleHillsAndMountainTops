045a71ee3a4732dbd67d25916d0b683191299998
john <likisongproduct@gmail.com>
2019-07-02 00:08:43 -0700

    moved everything from footer tag to Footer.vue

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -5,22 +5,9 @@
         <IntroPage />
         <div class="padding"></div>
         <FeaturedPage />
-        <div class="padding"></div>
         <PostPage />
-
-        <footer class="page-footer">
-            <div class="footer-content container-fluid">
-                <div class="row">
-                    <div class="col-md-12 py-5">
-                        <a href="https://www.facebook.com/john.song.5264382"><i class="fab fa-facebook"></i></a>
-                        <a href="https://www.instagram.com/john_song_/"><i class="fab fa-instagram"></i></a>
-                        <a href="https://github.com/jarhun88"><i class="fab fa-github"></i></a>
-                        <a href="https://www.youtube.com/channel/UCSJrG7j5AwuUcoD9hLetkmw?view_as=subscriber"><i
-                                class="fab fa-youtube"></i></a>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <div class="padding"></div>
+        <Footer />
     </div>
 </template>
 
@@ -29,6 +16,7 @@
     import IntroPage from "./components/IntroPage.vue";
     import FeaturedPage from "./components/FeaturedPage.vue";
     import PostPage from "./components/PostPage.vue";
+    import Footer from "./components/Footer.vue";
 
     export default {
         name: 'MainPage',
@@ -37,6 +25,7 @@
             IntroPage,
             FeaturedPage,
             PostPage,
+            Footer,
         },
         props: {
         }
@@ -64,20 +53,4 @@
         font-size: 20px;
     }
 
-    .footer-content {
-        background: #058484;
-    }
-
-    .fab {
-        padding: 10px;
-        font-size: 20px;
-    }
-
-    a {
-        color: white;
-    }
-
-    a:hover {
-        opacity: 0.5;
-    }
 </style>
