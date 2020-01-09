d21342e373440a6720222c49ae3ed7d0cca95af2
john <likisongproduct@gmail.com>
2020-01-08 22:47:48 -0800

    dynamic map switching

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,11 +1,63 @@
 <template>
     <div class="map">
         <div class="left-box">
-            <Showcase />
+            <div class="top-bar">
+                <div class="button-box">
+                    <v-btn v-on:click="setActiveTravel(Korea, items.Korea, KoreaLink)" rounded depressed class="button"
+                        width="150px" color=#C5C5C5>
+                        Korea</v-btn>
+                </div>
+                <div class="button-box">
+                    <v-btn v-on:click="setActiveTravel(Japan, items.Japan, JapanLink)" rounded depressed class="button"
+                        width="150px" color=#C5C5C5>
+                        Japan</v-btn>
+                </div>
+                <div class="button-box">
+                    <v-btn v-on:click="setActiveTravel(Peru, items.Peru, PeruLink)" rounded depressed class="button"
+                        width="150px" color=#C5C5C5>
+                        Peru</v-btn>
+                </div>
+                <div class="button-box">
+                    <v-btn v-on:click="setActiveTravel(Thailand, items.Thailand, ThailandLink)" rounded depressed
+                        class="button" width="150px" color=#C5C5C5>Thailand</v-btn>
+                </div>
+                <div class="button-box">
+                    <v-btn v-on:click="setActiveTravel(Hawaii, items.Hawaii)" rounded depressed class="button"
+                        width="150px" color=#C5C5C5>Hawaii</v-btn>
+                </div>
+                <div class="button-box">
+                    <v-btn v-on:click="setActiveTravel(Singapore, items.Singapore, SingaporeLink)" rounded depressed
+                        class="button" width="150px" color=#C5C5C5>Singapore</v-btn>
+                </div>
+            </div>
+            <v-divider class="mx-4"></v-divider>
+            <div class="bottom-bar">
+                <div class="wrapper-box">
+                    <div class="photo1-box">
+                        <v-carousel :show-arrows="false" height="100%" cycle hide-delimiter-background>
+                            <v-carousel-item contain v-for="(item,i) in activeImages" :key="i" :src="item.src">
+                            </v-carousel-item>
+                        </v-carousel>
+                    </div>
+                    <div class="detail-box">
+                        <div class="button-box">
+                            <v-btn v-on:click="show(description)" rounded depressed class="button" width="150px"
+                                color=#C5C5C5>
+                                About
+                                {{activeTravel}}</v-btn>
+                        </div>
+                        <div class="button-box">
+                            <v-btn v-bind:href="activeLink" rounded depressed class="button" width="150px"
+                                color=#C5C5C5>Gallery</v-btn>
+                        </div>
+                    </div>
+                </div>
+                <modals-container />
+            </div>
         </div>
         <div class="right-box">
             <div class="gmap-box">
-                <Map />
+                <Map v-bind:pan="start" v-bind:zoom="zoom"/>
             </div>
             <div class="sidebar-box">
                 <SideBar />
@@ -17,23 +69,203 @@
 <script>
     import SideBar from './SideBar.vue'
     import Map from './Map.vue'
-    import Showcase from './Showcase.vue'
 
     export default {
         name: 'MainPage',
         components: {
             SideBar,
             Map,
-            Showcase
         },
 
         data() {
             return {
-                
+                zoom: 3,
+                start: {
+                    lat: 10.3157,
+                    lng: 123.8854
+                },
+
+                activeImages: [
+                    {
+                        src: require('../images/korea.jpg')
+                    },
+                    {
+                        src: require('../images/taiwan.jpg')
+                    },
+                    {
+                        src: require('../images/singapore.jpg')
+                    },
+                    {
+                        src: require('../images/thailand.jpg')
+                    },
+                ],
+
+                items: {
+                    Korea: {
+                        images: [
+                            {
+                                src: require('../images/korea2.jpg')
+                            },
+                            {
+                                src: require('../images/korea3.jpg')
+                            },
+                        ],
+                        coords: {
+                            lat: 37.566536,
+                            lng: 126.977966
+                        }
+                    },
+                    Japan: {
+                        images: [
+                            {
+                                src: require('../images/japan.jpg')
+                            },
+                            {
+                                src: require('../images/japan2.jpg')
+                            },
+                            {
+                                src: require('../images/japan3.jpg')
+                            }
+                        ],
+                        coords: {
+                            lat: 35.689487,
+                            lng: 139.691711
+                        }
+                    },
+                    Peru: {
+                        images: [
+                            {
+                                src: require('../images/peru.jpg')
+                            },
+                            {
+                                src: require('../images/macchupicchu.jpg')
+                            },
+                            {
+                                src: require('../images/desert.jpg')
+                            },
+                            {
+                                src: require('../images/colcacanyon.jpg')
+                            },
+                            {
+                                src: require('../images/rainbowmountain.jpg')
+                            },
+                            {
+                                src: require('../images/casa.jpg')
+                            }
+                        ],
+                        coords: {
+                            lat: -14.0875,
+                            lng: -75.7626
+                        }
+                    },
+                    Thailand: {
+                        images: [
+                            {
+                                src: require('../images/thailand.jpg')
+                            },
+                            {
+                                src: require('../images/thailand1.jpg')
+                            },
+                            {
+                                src: require('../images/thailand4.jpg')
+                            }
+                        ],
+                        coords: {
+                            lat: 13.756331,
+                            lng: 100.501762
+                        }
+                    },
+                    Singapore: {
+                        images: [
+                            {
+                                src: require('../images/singapore.jpg')
+                            },
+                            {
+                                src: require('../images/singapore1.jpg')
+                            },
+                            {
+                                src: require('../images/singapore2.jpg')
+                            },
+                            {
+                                src: require('../images/singapore3.jpg')
+                            }
+                        ],
+                        coords: {
+                            lat: 1.352083,
+                            lng: 103.819839
+                        }
+                    },
+                    Hawaii: [
+                        {
+                            src: require('../images/peru.jpg')
+                        },
+                        {
+                            src: require('../images/taiwan.jpg')
+                        },
+                        {
+                            src: require('../images/singapore.jpg')
+                        },
+                        {
+                            src: require('../images/thailand.jpg')
+                        }
+                    ]
+
+                },
+                activeLink: "https://www.flickr.com/photos/182220016@N07/albums/72157710668016766",
+                KoreaLink: "https://www.flickr.com/photos/182220016@N07/albums/72157709253333771",
+                JapanLink: "https://www.flickr.com/photos/182220016@N07/albums/72157709253151606",
+                PeruLink: "https://www.flickr.com/photos/182220016@N07/albums/72157710668016766",
+                ThailandLink: "https://www.flickr.com/photos/182220016@N07/albums/72157709252746627",
+                HawaiiLink: "",
+                SingaporeLink: "https://www.flickr.com/photos/182220016@N07/albums/72157709252817771",
+
+                activeTravel: this.Korea,
+                Korea: "Korea",
+                Japan: "Japan",
+                Peru: "Peru",
+                Thailand: "Thailand",
+                Hawaii: "Hawaii",
+                Singapore: "Singapore",
+
+                description: {
+                    header: "Peru",
+                    para: "hello"
+                }
             }
         },
         methods: {
-            
+
+            show(description) {
+                this.$modal.show({
+                    template: `
+            <div>
+            <h1>{{ header }}</h1>
+            <p>{{ para }}</p>
+            </div>`,
+                    props: ['header', 'para']
+                }, {
+                    header: description.header,
+                    para: description.para
+                }, {
+                    height: 'auto'
+                }, {
+                    'before-close': () => { console.log('this will be called before the modal closes'); }
+                })
+            },
+
+            hide() {
+                this.$modal.hide('hello-world');
+            },
+
+            setActiveTravel(travel, country, link) {
+                this.activeTravel = travel
+                this.activeImages = country.images
+                this.activeLink = link
+                this.start = country.coords
+                this.zoom = 6
+                console.log(this.activeTravel)
+                console.log(this.activeImages)
+            }
         }
     }
 
@@ -82,5 +314,46 @@
 
     .fab {
         font-size: 600%;
+    }
+
+    .top-bar {
+        margin-top: 2%;
+        height: 20%;
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .bottom-bar {
+        height: 70%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    .photo1-box {
+        height: 80%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        max-height: 450px;
+    }
+
+    .wrapper-box {
+        width: 100%;
+        height: 100%;
+    }
+
+    .button-box {
+        margin: 1%;
+    }
+
+    .detail-box {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
     }
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,6 @@
 <template>
-    <GmapMap :center="start" :zoom="3" map-type-id="terrain" style="width: 100%; height: 100%;" :options="options">
+    <GmapMap ref="mapRef" :center="pan" :zoom="zoom" map-type-id="terrain" style="width: 100%; height: 100%;"
+        :options="options">
         <GmapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true"
             :draggable="true" @click="toggleInfoWindow(m,index)" />
         <gmap-info-window :options="infoOptions" :position="infoWindowPos" :opened="infoWinOpen"
@@ -13,204 +14,211 @@
     export default {
         name: 'Map',
         props: {
+            pan: Object,
+            zoom: Number
         },
+        mounted() {
+            // At this point, the child GmapMap has been mounted, but
+            // its map has not been initialized.
+            // Therefore we need to write mapRef.$mapPromise.then(() => ...)
+
+            this.$refs.mapRef.$mapPromise.then((map) => {
+                map.panTo({ lat: 1.38, lng: 103.80 })
+            })
+        },
+
         data() {
             return {
-                start: {
-                    lat: 10.3157,
-                    lng: 123.8854
-                },
                 options: {
                     disableDefaultUI: true,
-                    // scrollwheel: false,
                     minZoom: 3,
                     maxZoom: 10,
                 },
                 markers: [
-                {
-                    position: {
-                        name: "Abbotsford",
-                        description: "description 1",
-                        lat: 49.063042,
-                        lng: -122.251350
+                    {
+                        position: {
+                            name: "Abbotsford",
+                            description: "description 1",
+                            lat: 49.063042,
+                            lng: -122.251350
+                        },
                     },
-                },
-                {
-                    position: {
-                        name: "Vancouver",
-                        description: "description ",
-                        lat: 49.263569,
-                        lng: -123.138573
-                    }
-                },
-                {
-                    position: {
-                        name: "Busan",
-                        description: "description ",
-                        lat: 35.097000,
-                        lng: 129.008102
-                    }
-                },
-                {
-                    position: {
-                        name: "Seoul",
-                        description: "description ",
-                        lat: 37.566536,
-                        lng: 126.977966
-                    }
-                },
-                {
-                    position: {
-                        name: "Gyeungju",
-                        description: "description ",
-                        lat: 35.8562,
-                        lng: 129.2247
-                    }
-                },
-                {
-                    position: {
-                        name: "Pohang",
-                        description: "description ",
-                        lat: 36.0190,
-                        lng: 129.3435
-                    }
-                },
-                {
-                    position: {
-                        name: "Tongyeong",
-                        description: "description ",
-                        lat: 34.8544,
-                        lng: 128.4332
-                    }
-                },
-                {
-                    position: {
-                        name: "Tokyo",
-                        description: "description ",
-                        lat: 35.689487,
-                        lng: 139.691711
-                    }
-                },
-                {
-                    position: {
-                        name: "Osaka",
-                        description: "description ",
-                        lat: 34.6937,
-                        lng: 135.5023
-                    }
-                },
-                {
-                    position: {
-                        name: "Kyoto",
-                        description: "description ",
-                        lat: 35.0116,
-                        lng: 135.7681
-                    }
-                },
-                {
-                    position: {
-                        name: "Bangkok",
-                        description: "description ",
-                        lat: 13.756331,
-                        lng: 100.501762
-                    }
-                },
-                {
-                    position: {
-                        name: "Krabi",
-                        description: "description ",
-                        lat: 8.0855,
-                        lng: 98.9063
-                    }
-                },
-                {
-                    position: {
-                        name: "Phi Phi Islands",
-                        description: "description ",
-                        lat: 7.7407,
-                        lng: 98.7784
-                    }
-                },
-                {
-                    position: {
-                        name: "Singapore",
-                        description: "description ",
-                        lat: 1.352083,
-                        lng: 103.819839
-                    }
-                },
-                {
-                    position: {
-                        name: "Taipei",
-                        description: "description ",
-                        lat: 25.032969,
-                        lng: 121.565414
-                    }
-                },
-                {
-                    position: {
-                        name: "Honolulu",
-                        description: "description ",
-                        lat: 21.3069,
-                        lng: 157.8583
-                    }
-                },
-                {
-                    position: {
-                        name: "North Shore",
-                        description: "description ",
-                        lat: 21.5617,
-                        lng: 158.0716
-                    }
-                },
-                {
-                    position: {
-                        name: "Lima",
-                        description: "description ",
-                        lat: -12.046374,
-                        lng: -77.042793
-                    }
-                },
-                {
-                    position: {
-                        name: "Paracas",
-                        description: "description ",
-                        lat: -13.8409,
-                        lng: -76.2508
-                    }
-                },
-                {
-                    position: {
-                        name: "Huacachina",
-                        description: "description ",
-                        lat: -14.0875,
-                        lng: -75.7626
-                    }
-                },
-                {
-                    position: {
-                        name: "Arequipa",
-                        description: "description ",
-                        lat: -16.4090,
-                        lng: -71.5375
-                    }
-                },
-                {
-                    position: {
-                        name: "Cusco",
-                        description: "description ",
-                        lat: -13.5320,
-                        lng: -71.9675
-                    }
-                },
-                {
-                    position: {
-                        name: "Aguas Caliente",
-                        description: "description ",
-                        lat: -13.1547,
-                        lng: -72.5254
-                    }
-                },
+                    {
+                        position: {
+                            name: "Vancouver",
+                            description: "description ",
+                            lat: 49.263569,
+                            lng: -123.138573
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Busan",
+                            description: "description ",
+                            lat: 35.097000,
+                            lng: 129.008102
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Seoul",
+                            description: "description ",
+                            lat: 37.566536,
+                            lng: 126.977966
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Gyeungju",
+                            description: "description ",
+                            lat: 35.8562,
+                            lng: 129.2247
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Pohang",
+                            description: "description ",
+                            lat: 36.0190,
+                            lng: 129.3435
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Tongyeong",
+                            description: "description ",
+                            lat: 34.8544,
+                            lng: 128.4332
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Tokyo",
+                            description: "description ",
+                            lat: 35.689487,
+                            lng: 139.691711
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Osaka",
+                            description: "description ",
+                            lat: 34.6937,
+                            lng: 135.5023
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Kyoto",
+                            description: "description ",
+                            lat: 35.0116,
+                            lng: 135.7681
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Bangkok",
+                            description: "description ",
+                            lat: 13.756331,
+                            lng: 100.501762
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Krabi",
+                            description: "description ",
+                            lat: 8.0855,
+                            lng: 98.9063
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Phi Phi Islands",
+                            description: "description ",
+                            lat: 7.7407,
+                            lng: 98.7784
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Singapore",
+                            description: "description ",
+                            lat: 1.352083,
+                            lng: 103.819839
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Taipei",
+                            description: "description ",
+                            lat: 25.032969,
+                            lng: 121.565414
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Honolulu",
+                            description: "description ",
+                            lat: 21.3069,
+                            lng: 157.8583
+                        }
+                    },
+                    {
+                        position: {
+                            name: "North Shore",
+                            description: "description ",
+                            lat: 21.5617,
+                            lng: 158.0716
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Lima",
+                            description: "description ",
+                            lat: -12.046374,
+                            lng: -77.042793
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Paracas",
+                            description: "description ",
+                            lat: -13.8409,
+                            lng: -76.2508
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Huacachina",
+                            description: "description ",
+                            lat: -14.0875,
+                            lng: -75.7626
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Arequipa",
+                            description: "description ",
+                            lat: -16.4090,
+                            lng: -71.5375
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Cusco",
+                            description: "description ",
+                            lat: -13.5320,
+                            lng: -71.9675
+                        }
+                    },
+                    {
+                        position: {
+                            name: "Aguas Caliente",
+                            description: "description ",
+                            lat: -13.1547,
+                            lng: -72.5254
+                        }
+                    },
                 ],
 
                 infoContent: '',
@@ -228,6 +236,7 @@
             }
         },
         methods: {
+
             getPosition: function (marker) {
                 return {
                     lat: parseFloat(marker.lat),
@@ -264,6 +273,7 @@
                   </div>
                 </div>`);
             }
+
         }
     }
 </script>
